547414a56784290df4a5c77b3c0a38783116d619
myorn <lednovalex@gmail.com>
2024-07-21 23:22:25 +0300

    api boilerplate

--- app.py
+++ app.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route("/")
 def serve_front_page():
     return "Whateve"
+
+
+@app.route("/records")
+def get_records():
+    return "Whateve"
+
+
+@app.route("/record")
+def post_record():
+    return "Whateve"
+
+
+@app.route("/record")
+def edit_record():
+    return "Whateve"
+
+
+@app.route("/record")
+def delete_record():
+    return "Whateve"
